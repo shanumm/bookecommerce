922196838d665d133ef9faa2c2365eab4fd80900
Ektakaushik <105712212+Ektakaushik@users.noreply.github.com>
2023-03-21 18:12:47 +0530

    changess of footer

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -9,35 +9,35 @@ const FooterTop = () => (
         src="https://cdn-icons-png.flaticon.com/512/1828/1828754.png"
         alt=""
       />{" "}
-      Lorem, ipsum.
+      Duties and Taxes Guranteed
     </div>
     <div>
       <img
         src="https://cdn-icons-png.flaticon.com/512/1828/1828754.png"
         alt=""
       />{" "}
-      Lorem, ipsum.
+      Free Express Shipping
     </div>
     <div>
       <img
         src="https://cdn-icons-png.flaticon.com/512/1828/1828754.png"
         alt=""
       />{" "}
-      Lorem, ipsum.
+      Customer Love
     </div>
     <div>
       <img
         src="https://cdn-icons-png.flaticon.com/512/1828/1828754.png"
         alt=""
       />{" "}
-      Lorem, ipsum.
+      Easy Returns
     </div>
     <div>
       <img
         src="https://cdn-icons-png.flaticon.com/512/1828/1828754.png"
         alt=""
       />{" "}
-      Lorem, ipsum.
+      Secure Payment
     </div>
   </div>
 );
@@ -51,54 +51,54 @@ export default function Footer() {
           <img src={NDLogo} alt="" />
         </div>
         <div className="footerLinks">
-          <div>Heading</div>
+          <div>FEATURES</div>
           <div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
+            <div>All in One (KINDERGARTEN)</div>
+            <div>Chaman Urdu Khushkhati</div>
+            <div>Play Writing</div>
+            <div>Play Reading</div>
+            {/* <div>Menu</div> */}
           </div>
         </div>
         <div className="footerLinks">
-          <div>Heading</div>
+          <div>MENU</div>
           <div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
+            <div>About Us</div>
+            <div>Contact Us</div>
+            <div>MY aacount</div>
+            <div>blog</div>
+            <div>order history</div>
           </div>
         </div>
         <div className="footerLinks">
-          <div>Heading</div>
+          <div>CONTACT US</div>
           <div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
+            <div className="footer_highlight">Address</div>
+            <div>J-2/16 (BASEMENT) Padam chand Marg, Daryaganj</div>
+            <div className="footer_highlight">Office - 9310406659</div>
+            <div className="footer_highlight">Mobile - 9910365713</div>
+            <div className="footer_highlight">Email</div>
+            <div>Emails - support@nldr.in outreac@nldr.in</div>
+          </div>
+        </div>
+        <div className="footerLinks" id="footerSocialLinks">
+          <div>FOLLOW US</div>
+          <div>
+            <img src="https://cdn-icons-png.flaticon.com/128/3128/3128208.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/3128/3128212.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/733/733622.png" />
+            <img src="https://cdn-icons-png.flaticon.com/128/2111/2111491.png" />
           </div>
         </div>
         <div className="footerLinks">
-          <div>Heading</div>
-          <div>
+          <div>JOIN US</div>
+          {/* <div>
             <div>Menu</div>
             <div>Menu</div>
             <div>Menu</div>
             <div>Menu</div>
             <div>Menu</div>
-          </div>
-        </div>
-        <div className="footerLinks">
-          <div>Heading</div>
-          <div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
